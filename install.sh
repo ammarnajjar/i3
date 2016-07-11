92fa5d81e66c958611acb892bfd8ac44f24212da
Ammar Najjar <najjarammar@gmail.com>
2016-07-11 11:22:10 +0200

    add xclip installation

--- install.sh
+++ install.sh
@@ -9,7 +9,8 @@ function get_sudo() {
     fi
 }
 
-$SUDO dnf install -y i3 i3status i3lock xdotool zenity feh pavucontrol xdotool arandr rxvt-unicode tmux
+$SUDO dnf install -y i3 i3status i3lock xdotool zenity feh pavucontrol \
+	xdotool arandr rxvt-unicode tmux xclip
 $SUDO cp /usr/share/X11/xorg.conf.d/50-synaptics.conf /etc/X11/xorg.conf.d/
 git clone https://github.com/ammarnajjar/i3.git ~/.config/i3
 git clone git://github.com/vivien/i3blocks /tmp/i3blocks
